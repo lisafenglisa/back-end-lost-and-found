183fc58d33f5bc9e43204eae12b5afdc1e9499be
lisafeng <lisafenglisa@yahoo.com>
2023-01-26 11:28:35 -0500

    Revert "conncetion to database and wrote part of routes"
    This reverts commit 6754808346bfa2ef13ba5610f8f7254fe249cd6d.

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-const express = require("express");
-const app = express();
+const express=require("express"); 
+const app= express();
 const port = 3000;
-const items = require("./routes/items");
 
-// middleware
-
-app.use(express.json());
-
-// routes
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get("/", function(req,res){
+    res.send("Hello World!")
 });
-
-app.use("/api/v1/items", items);
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+        console.log(`Example app listening on port ${port}`);     
+})
