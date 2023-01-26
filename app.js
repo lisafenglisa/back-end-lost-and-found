6754808346bfa2ef13ba5610f8f7254fe249cd6d
lisafeng <lisafenglisa@yahoo.com>
2023-01-26 11:24:35 -0500

    conncetion to database and wrote part of routes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express=require("express"); 
-const app= express();
+const express = require("express");
+const app = express();
 const port = 3000;
+const items = require("./routes/items");
 
-app.get("/", function(req,res){
-    res.send("Hello World!")
+// middleware
+
+app.use(express.json());
+
+// routes
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
+
+app.use("/api/v1/items", items);
+
 app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);     
-})
+  console.log(`Example app listening on port ${port}`);
+});
