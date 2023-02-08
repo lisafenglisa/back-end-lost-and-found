4638efab7087f61cca33fd9a9316f1c3ce9c675d
lisafeng <lisafenglisa@yahoo.com>
2023-02-08 11:21:40 -0500

    fixed connection.js variables

--- connection.js
+++ connection.js
@@ -1,11 +1,20 @@
 const { Client } = require("pg");
+require("dotenv").config();
+
+// const client = new Client({
+//   host: "0.0.0.0",
+//   user: "postgres",
+//   port: 5432,
+//   password: "Y19870804f!",
+//   database: "lost_and_found_development",
+// });
 
 const client = new Client({
-  host: "0.0.0.0",
-  user: "postgres",
+  host: process.env.HOST,
+  user: process.env.USER,
   port: 5432,
-  password: "Y19870804f!",
-  database: "lost_and_found_development",
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 module.exports = client;
